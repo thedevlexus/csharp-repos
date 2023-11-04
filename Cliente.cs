b729d36a63885a40a60078c87704fee843abe8eb
Lexus <123604138+thedevlexus@users.noreply.github.com>
2023-11-04 16:50:40 -0300

    Add files via upload

---
+++ Cliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tela
+{
+    internal class Cliente
+    {
+        public string nome;
+        public double saldo;
+
+        public double depositoSaldo(double valor)
+        {
+            if (valor <= 0)
+            {
+                throw new Exception("Tem coisa errada ai parceiro.");
+            }
+
+            saldo += valor;
+            return saldo;
+        }
+        public double saqueSaldo(double valor)
+        {
+            if (valor <= 0 || saldo <= valor)
+            {
+                throw new Exception("Tem coisa errada ai parceiro.");
+            }
+            saldo -= valor;
+            return saldo;
+        }
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tela
+{
+
+    public partial class Banco : Form
+    {
+        Cliente cliente;
+        public Banco()
+        {
+            InitializeComponent();
+           this.cliente = new Cliente();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void simToolStripMenuItem5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void simToolStripMenuItem6_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbOperacao_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNome_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btDepositar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            double valor = Convert.ToDouble(txtOperacao.Text);
+            cliente.depositoSaldo(valor);
+            txtSaldo.Text = cliente.saldo.ToString(".00");
+            txtOperacao.Clear();
+            } catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+            
+
+                
+            
+        }
+
+        private void groupBox1_Enter_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtOperacao_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btCreditar_Click(object sender, EventArgs e)
+        {
+            try 
+            { 
+            double valor = Convert.ToDouble(txtOperacao.Text);
+            cliente.saqueSaldo(valor);
+            txtSaldo.Text = cliente.saldo.ToString(".00");
+            txtOperacao.Clear();
+            }
+            catch (Exception exception)
+            { 
+            MessageBox.Show(exception.Message);
+            }
+
+            txtOperacao.Clear();
+        }
+    }
+}
